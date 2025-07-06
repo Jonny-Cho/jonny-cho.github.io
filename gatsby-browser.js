04a4fefb1a536e26912a8be32160d192babd6ed4
Jonny Cho <andante2183@gmail.com>
2025-07-06 23:30:06 +0900

    Revert "feat: Google AdSense 통합 구현 (#15)" (#16)
    This reverts commit 3c784312b55824f3e01c7cd27d087f10f69ef3ea.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,1 @@
 require('typeface-montserrat');
-
-export const onClientEntry = () => {
-  const script = document.createElement('script');
-  script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6754483654653335';
-  script.async = true;
-  script.crossOrigin = 'anonymous';
-  document.head.appendChild(script);
-};
